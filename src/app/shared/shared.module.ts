d098d4ca41de47a93257688898a85d82b03c1c80
DaniilTulkin <tulkindaniilal@gmail.com>
2022-07-03 21:09:49 +0300

    plugin info component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,22 +4,25 @@ import { CommonModule } from '@angular/common';
 import { CollapseClosePanelComponent } from './components/collapse-close-panel/collapse-close-panel.component';
 import { WebView2Service } from './services/webview2.service';
 import { PluginsService } from './services/plugins.service';
+import { LineChartModule } from './modules/charts/line-chart/line-chart.module';
 
 
 
 @NgModule({
   declarations: [
-    CollapseClosePanelComponent
+    CollapseClosePanelComponent,
   ],
   providers: [
     WebView2Service,
     PluginsService
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    LineChartModule
   ],
   exports: [
-    CollapseClosePanelComponent
+    CollapseClosePanelComponent,
+    LineChartModule
   ]
 })
 export class SharedModule { }
