5ea91fbefa37fb66e126f78c1f38f7420645b867
DaniilTulkin <tulkindaniilal@gmail.com>
2022-07-05 00:01:08 +0300

    auth module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PluginsBrowserComponent } from './plugins-browser/plugins-browser.component';
 
+import { PluginsBrowserComponent } from './plugins-browser/plugins-browser.component';
+import { CollapseClosePanelComponent } from './shared/components/collapse-close-panel/collapse-close-panel.component';
 import { RoutersEnum } from './shared/enums/routers.enum';
 
 const routes: Routes = [
-  {path: RoutersEnum.PluginsBrowser, component: PluginsBrowserComponent}
+  {path: "", component: CollapseClosePanelComponent, children: [
+    {path: RoutersEnum.PluginsBrowser, component: PluginsBrowserComponent}
+  ]},
 ];
 
 @NgModule({
